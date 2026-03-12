914ad93535b02258bdc4014e58c0172b19ef9071
Fahmim Mohammod Shahriar <mfahim.shahriar50@gmail.com>
2026-03-12 23:54:25 +0300

    add file into middleware

--- app/main.py
+++ app/main.py
@@ -10,6 +10,7 @@ from app.infra.core.logging import get_logger
 from app.infra.db.database import check_db_connection
 from app.api.v1.routers import api_router
 
+
 from app.api.middleware import add_exception_handlers
 
 
@@ -27,7 +28,6 @@ def project_init() -> FastAPI:
         docs_url="/flowcare/docs",
         redoc_url="/flowcare/redoc",
     )
-
     add_exception_handlers(app)
 
     # Routers calling endpoint
